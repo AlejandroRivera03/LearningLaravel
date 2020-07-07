dfdbb1df1e15851cd7a7bfb2fb40ad5cc69e03f4
Alejandro <alejandro.rivera0309@gmail.com>
2020-07-06 23:53:30 -0500

    Sending emails (log emails now, try it with mailtrap to improve the email view)

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
 
 class MessagesController extends Controller
 {
     public function store(Request $request){
-        request()->validate([
+        $message = request()->validate([
             'name'      => 'required',
             'email'     => 'required|email',
             'subject'   => 'required',
@@ -16,6 +18,8 @@ class MessagesController extends Controller
             'name.required' => __('I need your name'), // Custom messages
         ]);
 
-        return 'Datos Validados';
+        Mail::to('alejandro.rivera0309@gmail.com')->queue(new MessageReceived($message));
+
+        return 'Mensaje enviado';
     }
 }
